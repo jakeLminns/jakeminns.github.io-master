bba3639f648dc0a04e1a4941c9dc0b1fc4a6512f
jakeLminns <jm2005@kent.ac.uk>
2017-05-30 00:25:42 +0100

    c
    c

---
+++ app.js
@@ -0,0 +1,155 @@
+var main = function() {
+
+
+
+        $('#Carousel-General').carousel({
+                interval: 40000
+        });
+
+ $('.button-0').addClass('active-button-0');
+
+         $('#Carousel-General').on('slide.bs.carousel', function (e) {
+
+                 var id = $('.item.active').data('slide-number');
+                $('.button-'+id).removeClass('active-button-'+id);
+
+
+        });
+
+        $('#Carousel-General').on('slid.bs.carousel', function (e) {
+
+                 var id = $('.item.active').data('slide-number');
+                $('.button-'+id).addClass('active-button-'+id);
+
+
+        });
+
+
+
+
+$('.project-link').click(function(){
+
+                    var id = $(this).data('project-number');
+
+                    for(var i = 0; i<= 4;i++){
+                        if(i != id){
+                             $('.projects-'+i).parent().toggleClass("float-right");
+                        }
+                    }
+
+                    if(id == $('.selected-proj-text').parent().data('project-number')){
+
+                    $('.selected-proj-button').toggleClass('col-md-3');
+                    $('.selected-proj-button').toggleClass('col-md-6');
+                    $('.selected-proj-button').removeClass('selected-proj-button');
+
+                    $('.selected-proj-text').toggleClass('proj-classification-hide')
+                    $('.selected-proj-text').removeClass('selected-proj-text');
+
+
+                    } else {
+
+                    $('.selected-proj-button').toggleClass('col-md-3');
+                    $('.selected-proj-button').toggleClass('col-md-6');
+                    $('.selected-proj-button').removeClass('selected-proj-button');
+
+                    $('.selected-proj-text').toggleClass('proj-classification-hide')
+                    $('.selected-proj-text').removeClass('selected-proj-text');
+
+
+                    $('.project-'+id).parent().toggleClass('selected-proj-button');
+                    $('.project-'+id).parent().toggleClass('col-md-3');
+                    $('.project-'+id).parent().toggleClass('col-md-6');
+
+                    $('.proj-text-'+id).toggleClass('proj-classification-hide');
+                    $('.proj-text-'+id).toggleClass('selected-proj-text');
+
+                    }
+
+
+});
+
+
+$(window).load( function(){
+
+    /* Check the location of each desired element */
+    $('.scroll-up').each( function(i){
+
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        /* If the object is completely visible in the window, fade it in */
+        if( bottom_of_window > bottom_of_object ){
+
+                                $(this).addClass('animated fadeInUp');
+
+        }
+
+    });
+
+});
+
+
+$(window).scroll( function(){
+
+    /* Check the location of each desired element */
+    $('.scroll-up').each( function(i){
+
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        /* If the object is completely visible in the window, fade it in */
+        if( bottom_of_window > bottom_of_object ){
+
+                                $(this).addClass('animated fadeInUp');
+
+        }
+
+    });
+
+});
+
+$('.soft-proj-dropdown').click(function(){
+
+         $(".classification-hide").toggleClass('proj-classification-hide');
+
+});
+
+$('.results-button').click(function(){
+
+         $(".result").toggleClass('proj-classification-hide');
+
+});
+$('.foundation-year-button').click(function(){
+
+         $(".foundation-year").toggleClass('proj-classification-hide');
+
+});
+
+$('.first-year-button').click(function(){
+
+         $(".first-year").toggleClass('proj-classification-hide');
+
+});
+
+$('.second-year-button').click(function(){
+
+         $(".second-year").toggleClass('proj-classification-hide');
+
+});
+
+$('.third-year-button').click(function(){
+
+         $(".third-year").toggleClass('proj-classification-hide');
+
+});
+$('.forth-year-button').click(function(){
+
+         $(".forth-year").toggleClass('proj-classification-hide');
+
+});
+
+
+};
+
+$(document).ready(main);
---
+++ flickr_app.js
@@ -0,0 +1,62 @@
+var apiKey = '8b539514ff55edcff58024d073a896e3';
+var xhr = new XMLHttpRequest();
+var apiurl = "https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=8b539514ff55edcff58024d073a896e3&user_id=127000141%40N04&format=json&nojsoncallback=1";
+
+
+var x = 0;
+var y = 0;
+
+var mydiv = document.getElementById("photo-slide");
+var widthHalf = mydiv.offsetWidth/2;
+
+
+    $.getJSON("https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=8b539514ff55edcff58024d073a896e3&user_id=127000141%40N04&extras=url_l&format=json&nojsoncallback=1",function(result){
+        console.log(result);
+
+      $.each(result.photos.photo, function(i, photo){
+        console.log(photo.width_l);
+
+        $.getJSON("https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=8b539514ff55edcff58024d073a896e3&photo_id=" +photo.id+ "&format=json&nojsoncallback=1", function(widthFetch){
+
+            $.each(widthFetch.sizes.size, function(y, photoSize){
+
+              if(photoSize.width >= widthHalf+200){
+
+                if(photoSize.height <= widthHalf+200){
+
+                console.log(widthFetch);
+
+                console.log(photoSize.source);
+
+
+
+
+                document.getElementById( +x+ "_flickr").style.background = "url('" +photoSize.source+ "')";
+                document.getElementById( +x+ "_flickr").style.backgroundSize = "auto " +(widthHalf+1)*0.72+ "px";
+                //document.getElementById( +x+ "_flickr").style.height = +photoSize.height+"px";
+                document.getElementById( +x+ "_flickr").style.width = +widthHalf+1+"px";
+                document.getElementById( +x+ "_flickr").style.height = +widthHalf+1+"px";
+
+
+
+
+                 document.getElementById( +x+ "_flickr").style.backgroundRepeat = "no-repeat";
+              //  document.getElementById( +x+ "_flickr").parentElement.style.height = +photoSize.height+"px";
+
+
+                 x=x+1;
+
+
+                 return false;
+                 return false;
+
+              
+              }
+            }
+
+            });
+          });
+      });
+    });
+
+
